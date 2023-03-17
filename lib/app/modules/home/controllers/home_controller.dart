4a88145514420919510e2d5c66361a00caaf1dff
Ali Yasin <110727129+mohammadaliyasin@users.noreply.github.com>
2023-03-18 00:17:51 +0530

    yasin

---
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+import '../views/functions/home_screen_function.dart';
+
+class HomeController extends GetxController {
+  
+// void onTap() async {
+//     await HomeFuntion.candidateDetails();
+//   }
+}
